6e1801290f662be51af4e4a1170a91321d7b47d4
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-13 07:03:20 +0500

    change

---
+++ testing/build/precache-manifest.20606e63d0c6f0865f24c09cfb296cfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214d58efa5c3196f86cf4e10fd4ad80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "039d08bceac095c663d0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a6b8a06ab0b64e390d5",
+    "url": "/static/js/2.8e14782c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e14782c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "039d08bceac095c663d0",
+    "url": "/static/js/main.26ddd0b1.chunk.js"
+  },
+  {
+    "revision": "9f10ad7796608ffb84b7",
+    "url": "/static/js/runtime-main.1b50fb31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
