3132d4bc2f1720db6b87160c8d8a1b5cc45b8330
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-13 07:06:40 +0500

    Update App.js

--- testing/src/App.js
+++ testing/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn jReact
+          Learn jhjReact
         </a>
       </header>
     </div>
