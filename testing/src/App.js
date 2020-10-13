d70378241978a608e9ac6b2519b832f35ac02bff
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-13 20:04:41 +0500

    changed virtual machine to window

--- testing/src/App.js
+++ testing/src/App.js
@@ -5,7 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>react p</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          my react ab9cde
+        </a>
+      </header>
     </div>
   );
 }
