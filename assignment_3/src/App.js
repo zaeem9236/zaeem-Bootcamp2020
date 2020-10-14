33886ea019c271131dffb3b1cebaa7d4c3f2db79
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-14 21:39:46 +0500

    App.js updated

--- assignment_3/src/App.js
+++ assignment_3/src/App.js
@@ -6,25 +6,29 @@ import PiaicCourses from './components/PIAIC_courses';
 function App() {
   return (
     <React.Fragment>
-      <div className='heading_div'>
-        <h1 className='heading'>PIAIC Courses</h1>
-        <hr />
-      </div>
+      <div className='main_body'>
 
-      <div className='AI' >
-        <PiaicCourses instructor='Muhammad Qasim' name='Artificial Intelligence' duration='1' />
-      </div>
+        <div className='heading_div'>
+          <h1 className='heading'>PIAIC Courses</h1>
+          <hr />
+        </div>
 
-      <div className="IOT">
-        <PiaicCourses instructor='Imran Ali' name='Internet of Things' duration='1.5' />
-      </div>
+        <div className='AI' >
+          <PiaicCourses instructor='Muhammad Qasim' name='Artificial Intelligence' duration='1' />
+        </div>
 
-      <div className='BC'>
-        <PiaicCourses instructor='Ahmad Manzoor' name='Blockchain' duration='1' />
-      </div>
+        <div className="IOT">
+          <PiaicCourses instructor='Imran Ali' name='Internet of Things' duration='1.5' />
+        </div>
 
-      <div className='CNC'>
-        <PiaicCourses instructor='Amir Pinger' name='Cloud Native Computing' duration='1' />
+        <div className='BC'>
+          <PiaicCourses instructor='Ahmad Manzoor' name='Blockchain' duration='1' />
+        </div>
+
+        <div className='CNC'>
+          <PiaicCourses instructor='Amir Pinger' name='Cloud Native Computing' duration='1' />
+        </div>
+
       </div>
     </React.Fragment>
   );
