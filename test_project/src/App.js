b9aed71f3e4e5cdbdc50314158347fd9cb4e9097
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-15 01:40:51 +0500

    App.js updated

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React,{useState} from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let[value, updateValue] = useState(0);
   return (
     <div className="App">
-      <h1>React project</h1>
+      <h1>Value is {value}</h1>
+      <hr />
+      <button onClick={() => {updateValue(value+1)}}>update value</button>
     </div>
   );
 }
