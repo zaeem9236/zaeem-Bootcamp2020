82fff75b894e25982c00dcbf7bc82797ec4b7a8c
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-14 16:41:46 +0500

    test_project updated

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and automation and much more...
+          test_dummy project
         </a>
       </header>
     </div>
