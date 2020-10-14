10786020474b2a01b4eb560356b5d33e15b7b334
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-15 03:11:58 +0500

    Update App.js

--- assignment-4/src/App.js
+++ assignment-4/src/App.js
@@ -5,7 +5,17 @@ import './App.css';
 function App() {
   return (
     <div className="main_div">
-      <h1>assignment-4</h1>
+      <div className='color_change'>
+        <button></button>
+      </div>
+
+      <div className='counter_section'>
+        <button className='decrement_button'> - </button>
+        <div className='counter'>
+          <h1 className='counter_text'>put counter here</h1>
+        </div>
+        <button className='increment_button'> + </button>
+      </div>
     </div>
   );
 }
