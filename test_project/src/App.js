3137da28de9b22333754121aae9fa314a3c17818
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-15 02:32:42 +0500

    App.js updated

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 
 function App() {
   let[value, updateValue] = useState(0);
+  let[color, updateColor] = useState(true);
   return (
-    <div className="App">
+    <div className={`App_${color ? '0' : '1' }`}>
       <h1>Value is {value}</h1>
       <hr />
+      <button onClick={() => {updateColor(!color)}}>Change color</button>
       <button onClick={() => {updateValue(value+1)}}>update value</button>
     </div>
   );
