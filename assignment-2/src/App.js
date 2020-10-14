79c4883205858f0bbc3326489e3c3b9c5739a0ef
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-14 15:34:24 +0500

    assignment-2 Project created and .yml modified

---
+++ assignment-2/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, my name is Zaeem Ahmad and assignment-2 is hosted via github actions</h1>
+    </div>
+  );
+}
+
+export default App;
