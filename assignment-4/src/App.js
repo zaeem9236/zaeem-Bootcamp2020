befeeb7e993f77d9fb687fd7bb1a98fddb725369
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-15 15:49:44 +0500

    App.js updated

--- assignment-4/src/App.js
+++ assignment-4/src/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [count, updateCount] = useState(0); 
+  let [nightMode, updateMode] = useState(true);
   return (
     <div className="main_div">
       <div className='color_change'>
-        <button className='color_change_button'>Change color to ...</button>
+  <button className='color_change_button' onClick={()=>{updateMode(!nightMode)}}>{nightMode ? "Light Mode" : "Dark Mode"}</button>
       </div>
 
       <div className='counter_section'>
-        <button className='decrement_button'> - </button>
+        <button className='decrement_button' onClick={()=>{
+          if(count > 0){
+            updateCount(count-1);
+          }
+        }}> - </button>
         <div className='counter'>
-          <h1 className='counter_text'>1</h1>
+          <h1 className='counter_text'>{count}</h1>
         </div>
-        <button className='increment_button'> + </button>
+        <button className='increment_button' onClick={() =>{
+          updateCount(count+1);
+        }}> + </button>
       </div>
     </div>
   );
