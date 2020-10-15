e5401245981854fa4d21cdbb0c4391d3ca3835ea
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-15 16:31:07 +0500

    update app.js

--- assignment-4/src/App.js
+++ assignment-4/src/App.js
@@ -6,21 +6,21 @@ function App() {
   let [count, updateCount] = useState(0); 
   let [nightMode, updateMode] = useState(true);
   return (
-    <div className="main_div">
+    <div className={`main_div ${nightMode ? 'main_div_night' : ''}`}>
       <div className='color_change'>
-  <button className='color_change_button' onClick={()=>{updateMode(!nightMode)}}>{nightMode ? "Light Mode" : "Dark Mode"}</button>
+  <button className={`color_change_button ${nightMode ? 'color_change_button_night' : ''}`} onClick={()=>{updateMode(!nightMode)}}>{nightMode ? "Light Mode" : "Dark Mode"}</button>
       </div>
 
       <div className='counter_section'>
-        <button className='decrement_button' onClick={()=>{
+        <button className={`decrement_button ${nightMode ? 'decrement_button_night' : ''}`} onClick={()=>{
           if(count > 0){
             updateCount(count-1);
           }
         }}> - </button>
         <div className='counter'>
-          <h1 className='counter_text'>{count}</h1>
+          <h1 className={`counter_text ${nightMode ? 'counter_text_night' : ''}`}>{count}</h1>
         </div>
-        <button className='increment_button' onClick={() =>{
+        <button className={`increment_button ${nightMode ? 'increment_button_night' : ''}`} onClick={() =>{
           updateCount(count+1);
         }}> + </button>
       </div>
