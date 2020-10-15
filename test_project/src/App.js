ebf7c85214e9860d07fc52fe5b9e9e1a820ea6a5
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-15 21:57:44 +0500

    updated css

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,26 +1,20 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    let[display, displayUpdate] = useState(false);
+  let [display, displayUpdate] = useState(false);
   return (
-    <div>
-      <button className='expand' onClick={()=> {displayUpdate(!display); console.log(display)} }>click to expand</button>
-      <div className = 'parent_headings'>
-      <h1 className='headings'>zaeem</h1>
-      <h1 className='headings'>zaeem</h1>
-      <h1 className='headings'>zaeem</h1>
-      <h1 className='headings'>zaeem</h1>
+    <div className='main_div'>
+      <div className='box'>
+        <div className='sub_box1'>
+          {/* <p>sub box</p> */}
+        </div>
+        <div className='sub_box2'>
+          {/* <p>sub box</p> */}
+        </div>
+        
       </div>
-      <div className={`list ${display ? 'list_none' : ''}`}>
-      <ul>
-        <li>zaeem</li>
-        <li>farhan</li>
-        <li>hassan</li>
-      </ul>
-      </div>
-
     </div>
   );
 }
