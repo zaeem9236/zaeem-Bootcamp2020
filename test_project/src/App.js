9ca5500e186bf47225da576faaaf863f1a6b40f3
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-19 22:02:33 +0500

    Update App.js

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -5,7 +5,7 @@ import globalStates from './globalStates.js';
 function App() {
   return(
     <React.Fragment>
-      <globalStates.Provider value={23}>
+      <globalStates.Provider value={123}>
       <Parent />
       </globalStates.Provider>
     </React.Fragment>
