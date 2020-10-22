7c4b1ab4bc976d02dda7aebfd816e1162842150d
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-23 01:22:02 +0500

    build bug fix

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Balance from './components/Balance';
 import Summary from './components/Summary';
 import TransactionHistory from './components/TranactionHistory';
-import AddTtransaction from './components/AddTransaction';
+import AddTransaction from './components/AddTransaction';
 import './App.css';
 
 function App() {
