ed2e25ccefaa8a097845a6b9801938dde712aaf3
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-23 22:02:54 +0500

    new Project-1

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import Balance from './components/Balance';
-import Summary from './components/Summary';
-import TransactionHistory from './components/TranactionHistory';
-import AddTransaction from './components/AddTransaction';
 import './App.css';
 
 function App() {
   return (
     <React.Fragment>
-      <div className='main_div'>
-        <Header />
-        <Balance />
-        <Summary />
-        <TransactionHistory />
-        <AddTransaction />
-      </div>
+      <h1>Expense Tracker</h1>
     </React.Fragment>
   );
 }
--- project-1/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-    return(
-        <React.Fragment>
-            <h2>Expense Tracker by Zaeem Ahmad</h2>
-        </React.Fragment>
-    );
-}
