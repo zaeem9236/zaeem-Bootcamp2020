5ecc6075c362ea27c6325ce24552ae6d4ecb49cc
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-25 03:43:40 +0500

    all is working except delete function

--- project-1/src/AppReducer.js
+++ project-1/src/AppReducer.js
@@ -1,9 +1,18 @@
+import TransactionHistory from "./components/TransactionHistory";
+
 const reducer = (state, action)=>{
     switch (action.type){
         case 'Add_Transaction':
-            return [action.payload,...state]
+            return [action.payload,...state];
+        case 'Delete_Transaction':{
+            // console.log (action.target.currentTarget.parentNode.parentNode);
+            // console.log(action.transactionsHistory);
+            // return [...state];
+            // console.log(delete action.transactionsHistory[action.index]);
+            console.log(delete state[action.index]);
+            }
         default:
-            return state
+            return state;
 
         
     }
