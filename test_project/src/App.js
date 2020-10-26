c3469dbb87f40b06a4be34d5211e0332adea37dc
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-26 22:46:21 +0500

    test_project updated

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,16 +1,11 @@
-import React, { useContext, useReducer } from 'react';
-import Parent from './Parent.js';
-import CentralisedData from './CentralisedData';
-import AppReducer from './AppReducer';
+import React from 'react';
 
 
 function App() {
-  let [state, dispatch] =useReducer(AppReducer, 12);
+
+
   return (
     <React.Fragment>
-        <CentralisedData.Provider value={[state, dispatch]}>
-        <Parent />
-        </CentralisedData.Provider>
     </React.Fragment>
   );
 }
