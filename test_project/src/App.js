53118f9d406b7b540587f931db3010be8831122a
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-28 13:40:54 +0500

    Update App.js

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 
 function App() {
-
+  
+  let promise = fetch('https://jsonplaceholder.typicode.com/todos/1');
+    promise.then(function(response){
+      return (response.json());
+    })
+    promise.then(function(response){
+      console.log(response);
+    })
   return (
     <React.Fragment>
-      <h1>class 8</h1>
+      <h1>fetching data...</h1>
     </React.Fragment>
   );
 }
