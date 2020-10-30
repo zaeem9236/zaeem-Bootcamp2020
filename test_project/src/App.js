545ea80e976902eaf1a8310514a3dfa104add1a1
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-30 15:07:26 +0500

    Update App.js

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -3,29 +3,9 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  let postdata = fetch('https://jsonplaceholder.typicode.com/posts',
-  {
-    method:'POST',
-    body: JSON.stringify({
-      title: 'xyz',
-      body: 'abc',
-      userId: 1
-    }),
-    headers:{
-      'Content-type': 'application/json; charset=UTF-8',
-    }
-  });
-  postdata.then((resolve)=>{
-    return ( resolve.json() );
-  })
-  .then((resolve)=>{
-    console.log(resolve);
-  });
-
-
   return (
     <React.Fragment>
-      <h1>App.js</h1>
+      <h1>Class 9</h1>
     </React.Fragment>
   );
 }
