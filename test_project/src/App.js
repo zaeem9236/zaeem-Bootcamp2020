e41c990baeb85b92fc5fb0d442781f1da6aaf38e
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-30 22:18:47 +0500

    Update App.js

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Piechart from './Piechart';
 
 
 function App() {
   return (
     <React.Fragment>
-      <Piechart></Piechart>
+      <h1 className='heading_1'> heading1 </h1>
+      <h2 className='heading_2'> heading2 </h2>
+      <h3 className='heading_3'> heading3 </h3>
+      <h4 className='heading_4'> heading4 </h4>
+      <h5 className='heading_5'> heading5 </h5>
+      <h6 className='heading_6'> heading6 </h6>
     </React.Fragment>
   );
 }
