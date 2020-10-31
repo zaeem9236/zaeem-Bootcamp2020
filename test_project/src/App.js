39f4b3363c258b60626331a67f3bd2177c150993
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-10-31 19:40:58 +0500

    updated

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
+import './App.css';
 
 
 function App() {
   return (
     <React.Fragment>
+      <div className='main_div'>
       <h1 className='heading_1'> heading1 </h1>
-      <h2 className='heading_2'> heading2 </h2>
-      <h3 className='heading_3'> heading3 </h3>
-      <h4 className='heading_4'> heading4 </h4>
-      <h5 className='heading_5'> heading5 </h5>
-      <h6 className='heading_6'> heading6 </h6>
+      </div>
     </React.Fragment>
   );
 }
