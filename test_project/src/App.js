451acca3eb80ccd68f3d442b66dc37abd5d5ad36
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-01 12:30:11 +0500

    Update App.js

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -3,10 +3,29 @@ import './App.css';
 
 
 function App() {
+
+
+
+
   return (
     <React.Fragment>
       <div className='main_div'>
-      <h1 className='heading_1'> heading1 </h1>
+        <h1 className='heading_1'> heading1 </h1>
+        <button onClick={() => {
+          fetch('https://project-wm12375.firebaseio.com/iotData/.json', {
+            method: 'GET',
+            // body: JSON.stringify({
+            //   title: 'foo',
+            //   body: 'bar',
+            //   userId: 1,
+            // }),
+            headers: {
+              'Content-type': 'application/json; charset=UTF-8',
+            },
+          })
+            .then((response) => response.json())
+            .then((json) => console.log(json))
+        }}>get Data</button>
       </div>
     </React.Fragment>
   );
