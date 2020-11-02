be8d55461f89e1145dd202f0a80b6a62318f2e5a
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-02 19:46:24 +0500

    summary view updated

--- project-2/src/components/SummaryView.js
+++ project-2/src/components/SummaryView.js
@@ -2,34 +2,39 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { FullscreenExit } from '@material-ui/icons';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        maxWidth: 800,
+        flexGrow: 1,
+        maxWidth: '1000px',
         margin: '0 auto'
     },
     paper: {
         padding: theme.spacing(2),
         textAlign: 'center',
         color: theme.palette.text.secondary,
-        maxWidth: 800,
     },
+    gridItems: {
+        border: '1px solid Red',
+        // maxWidth: '300px',
+    }
 }));
+
 
 function SummaryView() {
     const classes = useStyles();
+
     return (
         <div className={classes.root}>
-            <Grid container spacing={3} >
-                <Grid item xs={12} sm={4}>
-                    <Paper className={classes.paper}>xs=12</Paper>
+            <Grid container spacing={0}>
+                <Grid item xs={12} sm={4} className={classes.gridItems}>
+                    <Paper className={classes.paper}>View 1</Paper>
                 </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Paper className={classes.paper}>xs=12</Paper>
+                <Grid item xs={12} sm={4} className={classes.gridItems}>
+                    <Paper className={classes.paper}>View 2</Paper>
                 </Grid>
-                <Grid item xs={12} sm={4}>
-                    <Paper className={classes.paper}>xs=12</Paper>
+                <Grid item xs={12} sm={4} className={classes.gridItems}>
+                    <Paper className={classes.paper}>View 3</Paper>
                 </Grid>
             </Grid>
         </div>
