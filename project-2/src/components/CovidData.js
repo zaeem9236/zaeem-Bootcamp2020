16a1c6549f7bdbc01deb12b9029634a518f69fca
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-08 01:24:12 +0500

    production bug fix

--- project-2/src/components/CovidData.js
+++ project-2/src/components/CovidData.js
@@ -4,18 +4,20 @@ import React, { useEffect } from 'react';
 function CovidData(props) {
 //   let set_GlobalCovidData = props.setGlobalCovidData();
     
-function fetchBusinesses() {
-    fetch('https://api.covid19api.com/summary')
-    .then(response => response.json())
-    .then(json => props.setGlobalCovidData(json))
-  }
+
 
     useEffect(() => {
 
         
           fetchBusinesses()
+
+          function fetchBusinesses() {
+            fetch('https://api.covid19api.com/summary')
+            .then(response => response.json())
+            .then(json => props.setGlobalCovidData(json))
+          }
         
-    },[fetchBusinesses]);
+    },[]);
 
 
     return (
