ae6d3e51c92276de7213fdcb19e765dc99055437
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-15 18:40:49 +0500

    production bug fix

--- project-3/src/App.js
+++ project-3/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState,createContext } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Routing from './Components/Routing';
 import Navbar from './Components/Navbar';
 
