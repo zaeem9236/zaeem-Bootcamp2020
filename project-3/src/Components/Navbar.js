29e5d36e24426119eec9c3eb1a75f12f2557de6d
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-15 18:43:29 +0500

    production bug fix

--- project-3/src/Components/Navbar.js
+++ project-3/src/Components/Navbar.js
@@ -3,13 +3,9 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
 import { Link } from 'react-router-dom';
 import {useLocation} from 'react-router-dom'
-import { NoEncryption } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
