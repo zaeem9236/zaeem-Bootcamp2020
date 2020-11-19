623b7d97f05135cca7228f0b2b53a572fa948581
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-19 17:27:59 +0500

    WAAPI npm installed

--- project-4a/src/App.js
+++ project-4a/src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
+import useWebAnimations from "@wellyshen/use-web-animations";
 
-function App() {
+const App = () => {
+  const { ref, playState } = useWebAnimations({
+    keyframes: {
+      transform: ["translateX(500px)"], // Move by 500px
+      background: ["red", "blue", "green"], // Go through three colors
+    },
+    timing: {
+      delay: 500, // Start with a 500ms delay
+      duration: 1000, // Run for 1000ms
+      iterations: 2, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    },
+    onReady: ({ playState, animate, animation }) => {
+      // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+    },
+    onUpdate: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the running state or changes state
+    },
+    onFinish: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+    },
+    // More useful options...
+  });
+
   return (
-    <div className="App">
-      <h1>project 4A</h1>
+    <div className="container">
+      <p>🍿 Animation is {playState}</p>
+      <div className="target" ref={ref} />
     </div>
   );
-}
+};
 
-export default App;
+export default App
