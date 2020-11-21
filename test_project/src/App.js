587209103b7caf2170b959b3ab2d30f1f6f27625
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-22 03:23:55 +0500

    use reducer function for simple state tested

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
+function reducer(state, action){
+  switch(action){
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+      default:
+      return state;
+  }
+}
+
+let initialState = 0;
 
 function App() {
+  let [count,dispatch] = useReducer(reducer, initialState);
   return (
     <React.Fragment>
-      <h3>use Reducer</h3>
+      <h3>value: {count}</h3>
+      <button onClick={()=>{dispatch('increment')}}>Increment</button>
+      <button onClick={()=>{dispatch('decrement')}}>Decrement</button>
     </React.Fragment>
   );
 
