8442bc00e35b5c19d7127b52407f352cd0c0a76c
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-22 03:25:32 +0500

    useReducer testing with complex state

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,26 +1,14 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import './App.css';
 
-function reducer(state, action){
-  switch(action){
-    case 'increment':
-      return state + 1;
-    case 'decrement':
-      return state - 1;
-      default:
-      return state;
-  }
-}
+
 
 let initialState = 0;
 
 function App() {
-  let [count,dispatch] = useReducer(reducer, initialState);
   return (
     <React.Fragment>
-      <h3>value: {count}</h3>
-      <button onClick={()=>{dispatch('increment')}}>Increment</button>
-      <button onClick={()=>{dispatch('decrement')}}>Decrement</button>
+      <h1>useReducer testing with complex state</h1>
     </React.Fragment>
   );
 
