a9e921a61512fa19de0e775637c8d1ad0afe9fbf
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-22 03:18:11 +0500

    use reducer again

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import useWebAnimations from "@wellyshen/use-web-animations";
+
 
 function App() {
-
   return (
     <React.Fragment>
-      <h1>use Reducer</h1>
+      <h3>use Reducer</h3>
     </React.Fragment>
   );
 
