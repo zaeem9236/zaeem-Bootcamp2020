d2d03de6c83d798db154d83627426494906872e9
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-22 18:57:51 +0500

    use Reducer with mutli state and actions

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,37 +1,11 @@
-import React, { useReducer } from 'react';
+import React from 'react';
 import './App.css';
 
-function reducer(state, action){
-  switch(action.type){
-    case 'voltage_increment':
-      return {...state, voltage: state.voltage + 1};
-    case 'voltage_decrement': 
-      return {...state, voltage: state.voltage - 1};
-    case 'current_increment':
-      return {...state, current: state.current + 1};
-    case 'current_decrement': 
-      return {...state, current: state.current - 1};
-    default:
-      return state;
-  }
-}
-
-let initialState = {
-  voltage: 5,
-  current: 1
-};
 
 function App() {
-  let[parameter, dispatch] = useReducer(reducer, initialState);
   return (
     <React.Fragment>
-      <h1>Voltage: {parameter.voltage}</h1>
-      <button onClick={()=>{dispatch({type: 'voltage_increment'})}}>Increment</button>
-      <button onClick={()=>{dispatch({type: 'voltage_decrement'})}}>Decrement</button>
-      <hr />
-      <h1>Current: {parameter.current}</h1>
-      <button onClick={()=>{dispatch({type: 'current_increment'})}}>Increment</button>
-      <button onClick={()=>{dispatch({type: 'current_decrement'})}}>Decrement</button>
+      <h1>use Reducer with mutli state and actions</h1>
     </React.Fragment>
   );
 
