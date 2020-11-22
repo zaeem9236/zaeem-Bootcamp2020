ab426e473e23f2c5962b1e77c73c6d26457c40c3
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-22 17:43:17 +0500

    use reducer revision 2

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
 
+function reducer(state, action){
+  switch(action){
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+      default:
+        return state;
+  }
+}
 
 let initialState = 0;
 
 function App() {
+  let[updatedState, dispatch] = useReducer(reducer, initialState);
   return (
     <React.Fragment>
-      <h1>useReducer testing with complex state</h1>
+      <h1>Counter: {updatedState}</h1>
+      <button onClick={()=>{dispatch('increment')}}>Increment</button>
+      <button onClick={()=>{dispatch('decrement')}}>Secrement</button>
     </React.Fragment>
   );
 
