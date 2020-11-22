6faf99116a73671e32126cf96c0c85794fd88a21
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-22 18:57:02 +0500

    use Reducer with multiple states and actions tested

--- test_project/src/App.js
+++ test_project/src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 
+function reducer(state, action){
+  switch(action.type){
+    case 'voltage_increment':
+      return {...state, voltage: state.voltage + 1};
+    case 'voltage_decrement': 
+      return {...state, voltage: state.voltage - 1};
+    case 'current_increment':
+      return {...state, current: state.current + 1};
+    case 'current_decrement': 
+      return {...state, current: state.current - 1};
+    default:
+      return state;
+  }
+}
 
+let initialState = {
+  voltage: 5,
+  current: 1
+};
 
 function App() {
+  let[parameter, dispatch] = useReducer(reducer, initialState);
   return (
     <React.Fragment>
-      <h1>Current and voltage example with complex state management</h1>
+      <h1>Voltage: {parameter.voltage}</h1>
+      <button onClick={()=>{dispatch({type: 'voltage_increment'})}}>Increment</button>
+      <button onClick={()=>{dispatch({type: 'voltage_decrement'})}}>Decrement</button>
+      <hr />
+      <h1>Current: {parameter.current}</h1>
+      <button onClick={()=>{dispatch({type: 'current_increment'})}}>Increment</button>
+      <button onClick={()=>{dispatch({type: 'current_decrement'})}}>Decrement</button>
     </React.Fragment>
   );
 
