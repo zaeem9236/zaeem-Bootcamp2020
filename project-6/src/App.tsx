2fd6be73676307a90d27ba8342f91ec087e5b0f9
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-11-25 18:27:03 +0500

    type script update

--- project-6/src/App.tsx
+++ project-6/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 
+function show(obj: {name:string, age: number, jobless: boolean}){
+  console.log(obj.name,'yr');
+}
+
 function App() {
+let Data = {
+  name: 'zaeem',
+  age: 26,
+  jobless: true
+}
   return (
     <div className="App">
-      <h1>type script testing</h1>
+       {show(Data)};
     </div>
   );
 }
