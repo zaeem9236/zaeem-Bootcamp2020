adbc6cea7daf68f05eb54f2155906ff3838a937f
zaeem9236 <60064010+zaeem9236@users.noreply.github.com>
2020-12-02 21:09:40 +0500

    Timer component made

---
+++ project-7b/src/Components/Timer/Timer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Timer(){
+    return(
+        <React.Fragment>
+            <h1>I am timer component</h1>
+        </React.Fragment>
+    );
+}
+
+export default Timer;
